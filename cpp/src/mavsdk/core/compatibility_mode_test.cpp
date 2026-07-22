fbd95e6f416079f8e304cf59474398ff7a903480
Bartok9 <danielrpike9@gmail.com>
2026-07-22 04:51:10 -0400

    test(core): cover CompatibilityMode stream operator
    Lock ostream labels for all CompatibilityMode values plus unknown fallback.
    
    Signed-off-by: Bartok9 <danielrpike9@gmail.com>

---
+++ cpp/src/mavsdk/core/compatibility_mode_test.cpp
@@ -0,0 +1,34 @@
+#include "compatibility_mode.hpp"
+#include <gtest/gtest.h>
+#include <sstream>
+
+using namespace mavsdk;
+
+TEST(CompatibilityMode, StreamOperator)
+{
+    {
+        std::ostringstream oss;
+        oss << CompatibilityMode::Auto;
+        EXPECT_EQ(oss.str(), "Auto");
+    }
+    {
+        std::ostringstream oss;
+        oss << CompatibilityMode::Pure;
+        EXPECT_EQ(oss.str(), "Pure");
+    }
+    {
+        std::ostringstream oss;
+        oss << CompatibilityMode::Px4;
+        EXPECT_EQ(oss.str(), "Px4");
+    }
+    {
+        std::ostringstream oss;
+        oss << CompatibilityMode::ArduPilot;
+        EXPECT_EQ(oss.str(), "ArduPilot");
+    }
+    {
+        std::ostringstream oss;
+        oss << static_cast<CompatibilityMode>(42);
+        EXPECT_EQ(oss.str(), "Unknown");
+    }
+}
